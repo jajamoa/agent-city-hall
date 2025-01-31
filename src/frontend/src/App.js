6e625d0ec1239f9e2ed32ab6bb3d3432c833e2ab
LeeJAJA <jiajie@mit.edu>
2025-01-31 11:59:43 -0500

    updated style

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CommentVisualizer from './components/CommentVisualizer';
 import './styles/CommentVisualizer.css';
+import './styles/darkTheme.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Agent City Hall</h1>
+      {/* <h1 className="app-title">Agent City Hall</h1> */}
       <CommentVisualizer />
     </div>
   );
